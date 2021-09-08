5a41edb6488da2cc15caf211364d2d538a82d206
sholichanisaa <sholichanisaa@gmail.com>
2021-09-08 20:40:07 +0700

    Create and Update

--- src/errors.js
+++ src/errors.js
@@ -7,6 +7,16 @@ let getMessage = (code) =>{
         udf001 : "Invalid userid",
         udf002 : "Invalid projectid",
         udf003 : "Invalid taskid",
+        udf004 : "Cannot find data",
+        udf005 : "Authentication failed",
+        udf006 : "Project title is required",
+        udf007 : "Project status cannot empty",
+        udf008 : "Project description is required",
+        udf009 : "Project deadline cannot empty",
+        udf010 : "Task title is required",
+        udf011 : "Task status cannot empty",
+        udf012 : "Task description is required",
+        udf013 : "Task deadline cannot empty",
     }
     let sendObj = {
         message : messages[code]

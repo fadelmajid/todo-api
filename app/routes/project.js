91498056c52ae191610822e824800fd5b1cdc2b1
StevenDennis15 <70801715+StevenDennis15@users.noreply.github.com>
2021-09-09 10:38:29 +0700

    Update routes
    Switch auth to manual in controller

--- app/routes/project.js
+++ app/routes/project.js
@@ -4,9 +4,9 @@ module.exports = (app) => {
     const projectController = require("../controller/project")()
 
     let pRoutes = [
-        { method: 'get', route: '/userid', inits: [], middlewares: [projectController.getAllProject], auth: 'login' },
+        { method: 'get', route: '/userid', inits: [], middlewares: [projectController.getAllProject], auth: 'no' },
         // {method: 'post', route: '/', inits: [], middlewares: [projectController.createProject], auth: 'no'},
-        { method: 'get', route: '/:projectid', inits: [], middlewares: [projectController.getProject], auth: 'login' },
+        { method: 'get', route: '/:projectid', inits: [], middlewares: [projectController.getProject], auth: 'no' },
     ]
     return pRoutes
 }
